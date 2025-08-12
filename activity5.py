c324c3851da2e40b6f650a80886ef44f8b7ae6e6
ly8yvee <roshlydesquitado91@gmail.com>
2025-08-12 14:52:18 +0800

    Update activity5.py

--- activity5.py
+++ activity5.py
@@ -1,3 +1,2 @@
-name = input("Type any character here ...")
-
-print("The data type of name is ", )
+name = eval(input("Type any character here ..."))
+print("The data type of name is ", type(name))
