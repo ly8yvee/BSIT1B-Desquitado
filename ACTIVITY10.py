422aa681f0e61fc04d94038ef845fdf988d03318
ly8yvee <roshlydesquitado91@gmail.com>
2025-08-27 23:48:11 +0800

    ACTIVITY10.py

---
+++ ACTIVITY10.py
@@ -0,0 +1,10 @@
+USERNAME = "SHOEI"
+PASSWORD = "ARA8ARA"
+
+UNAME = input("ENTER USERNAME ---> ")
+PWORD = input("ENTER PASSWORD ---> ")
+
+if USERNAME == UNAME and PASSWORD == PWORD:
+            print("ACCESS GRANTED")
+else: 
+            print("ACCESS DENIED")
