f82dd7b79ec42e30a62a90908af4d33499af8bbc
ly8yvee <roshlydesquitado91@gmail.com>
2025-09-17 15:10:58 +0800

    code_challenge9.py

---
+++ code_challenge9.py
@@ -0,0 +1,5 @@
+libag = input("What do u want the parrot to say ---> ")
+num = eval(input("How many times do u want him to repeat it ---> "))
+
+for lybag in range(1, num, +1):
+    print("squawk", libag)
