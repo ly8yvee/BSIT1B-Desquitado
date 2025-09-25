1cc41617f7d41a98925ef908fd0f9656eb8cd512
ly8yvee <roshlydesquitado91@gmail.com>
2025-09-25 19:07:29 +0800

    activity18.py

---
+++ activity18.py
@@ -0,0 +1,9 @@
+for lybag in range(1,11,1):
+    for x in range(1, lybag ,1):
+        print(x, end=" ")
+    print(i)
+
+for libag in range(1,11,1):
+    for x in range(1, libag ,1):
+        print("*", end=" ")
+    print()
