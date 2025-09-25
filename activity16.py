74e20e09496284914f655aa636dc532f800467e5
ly8yvee <roshlydesquitado91@gmail.com>
2025-09-25 19:03:29 +0800

    activity16.py

--- activity16.py
+++ activity16.py
@@ -1,6 +1,2 @@
-print("Happy Hell Week",end="") 
-print("Ice Coffee na lang ang lahat ng activities",end="") 
-print("Hola mga Amigos")
-
-
-for lybag in range(1, 11, 1): print(lybag,end=" ") #add space
+for lybag in range(1, 11, 1):
+    print(lybag,end=" ")
