72d6f90795fe55212caec09093671ac2bbca4078
ly8yvee <roshlydesquitado91@gmail.com>
2025-09-25 19:06:02 +0800

    activity17.py

--- activity17.py
+++ activity17.py
@@ -1,8 +1,3 @@
-for lybag in range(1, 11, 1): 
-    for x in range(1, 11, 1):
-    print(x, end="") # 123 4 5 6 7 8 9 10 print(lybag) 
-
-
 for lybag in range(1, 11, 1):
     print(lybag,end=" - ")
     for x in range(1, 11, 1):
