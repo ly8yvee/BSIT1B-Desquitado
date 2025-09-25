7e81f0379426acf92b687fb132d1e561cb81e071
ly8yvee <roshlydesquitado91@gmail.com>
2025-09-25 13:20:02 +0800

    activity15.py

---
+++ activity15.py
@@ -0,0 +1,17 @@
+fname = 'Rusle' 
+mname = 'Lybag' 
+lname = 'Dekitado' 
+print(f"My name is {fname} {mname} {lname}")
+
+
+fname = 'Rusle'
+mname = 'Lybag'
+lname = 'Dekitado'
+print(f"My name is {fname.upper()} {mname.upper()} {lname.upper()} {4+4}")
+
+
+sum = 0
+for lybag in range(1, 9, 1):
+    x = eval(input(f"{lybag} - Input a number ---> "))
+    sum += X
+print(f"The sum of all the number given is (sum}")
