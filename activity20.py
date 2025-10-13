ff340bc8e5a5aae6ff584565a68ecf1e97c82bcb
ly8yvee <roshlydesquitado91@gmail.com>
2025-10-13 12:54:59 +0800

    activity20.py

---
+++ activity20.py
@@ -0,0 +1,6 @@
+for lybag in range(1, 11, 1):
+    for lee in range(1, lybag, 1):
+        print("* _ *", end=" ")
+    for bug in range(10, lybag, -1):
+        print("* - *", end=" ")
+    print()
