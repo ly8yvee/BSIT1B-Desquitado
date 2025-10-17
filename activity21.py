2c04ad135cc9dfdd8512a17d5acbeeeba27fef0c
ly8yvee <roshlydesquitado91@gmail.com>
2025-10-17 14:36:14 +0800

    activity21.py

---
+++ activity21.py
@@ -0,0 +1,11 @@
+d = True 
+
+while d True: 
+    r = input('do you want me to continue washing your machine? (yes / no) ').lower() 
+
+    if r == 'yes':  
+         print('continue washing your machine......')
+         continue 
+    else: 
+         print('your machine stop washing.....') 
+         break
