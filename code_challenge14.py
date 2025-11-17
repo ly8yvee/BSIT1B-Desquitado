f93e8ac6dabcb643dd42384b894b8fda931d5d63
ly8yvee <roshlydesquitado91@gmail.com>
2025-11-17 15:49:01 +0800

    Add files via upload

--- code_challenge14.py
+++ code_challenge14.py
@@ -1,20 +1,63 @@
-r = []
+import os
 
-lybag = ""
 
-print(" --ANIME TITLES COLLECTOR!-- ")
-print("Type 'done' to finish collecting. \n")
+os.system('cls')
+print('STUDENT INFORMATION SYSTEM')
+print('=================================')
 
-while lybag != "done":
-    
-    lybag = input("Enter an anime name: ")
-    
-    if lybag != "done":
-        r.append(lybag)
-        print(f"'{lybag}' has been added to your anime list.")
+#empty dictionary
+student_record = {}
 
-print("\n--- DONE COLLECTING ---")
+while True:
+    print("STUDENT FROM THE FOLLOWING OPTIONS")
+    print('A - ADD STUDENT RECORD')
+    print('B - PRINT ALL STUDENT RECORD')
+    print('C - SEARCH STUDENT RECORD')
+    print('D - DELETE STUDENT RECORD')
+    print('E - EDIT STUDENT RESCORD')
+    print('F - EXPORT STUDENT RECORD')
+    print('G - EXIT SYSTEM')
 
-print("Your Favorite Anime List:")
-for title in r:
-    print(f"- {title}")
+    choice = input('SELECT FROM THE OPTIONS ABOVE ---> ').lower()
+
+    if choice == 'a':
+        print('\nADDING STUDENT RECORD ')
+
+        id_no = input('please input ID number ---> ')
+
+        first_name = input('pleaase input Styudent First Name ---> ').upper()
+        second_name = input('please input Student Second Name ---> ').upper()
+        age = eval(input('input student age ---> '))
+        course = input('input student course ---> ').upper()
+        section = input('input student section ---> ').upper()
+
+        student_record[id_no] = [first_name, second_name, age, course, section]
+        print('DATA SAVED SUCCESSFULLY')
+
+
+        continue
+    elif choice == 'b':
+        print('PRINTING STUDENT RECORD')
+        # print('student_record')
+
+        for a, r in student_record.items():
+            print(f'STUDENT ID - {a}, INFORMATION - {r}')
+        continue
+    elif choice == 'c':
+        pass
+        continue
+    elif choice == 'd':
+        pass
+        continue
+    elif choice == 'e':
+        pass
+        continue
+    elif choice == 'f':
+        pass
+        continue
+    elif choice == 'g':
+        print('SYSTEM EXIT')
+        break
+    else:
+        print('INVALID CHOOICE, REENTER YOUR CHOICE')
+        continue
