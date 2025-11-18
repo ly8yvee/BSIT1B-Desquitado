ea5c32f72c63f63b1f14c1d4855ae4cf2701a699
ly8yvee <roshlydesquitado91@gmail.com>
2025-11-18 14:45:20 +0800

    activity23.py

--- activity23.py
+++ activity23.py
@@ -3,20 +3,21 @@ fruits2 = 'Inigo'
 
 fruits = ['Anton', 'Inigo', 'Joaquin', 'Clifford', 'Marco', 'Lee']
             #0       #1        #2           #3       #4       #5
-# print(fruits)
-# print(fruits[0])
-# print(fruits[0 : 3])
+print(fruits)
+print(fruits[0])
+print(fruits[0 : 3])
 
-# fruits.append('Heath')
-# print(fruits)
+fruits.append('Heath')
+print(fruits)
 
-# fruits.insert(3 , 'Miguel')
-# print(fruits)
+fruits.insert(3 , 'Miguel')
+print(fruits)
 
-# fruits.pop()
-# print(fruits)
+fruits.pop()
+print(fruits)
 
 fruits.len()
 print(fruits)
+
 
 
